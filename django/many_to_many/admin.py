2774014896016cd37de3a48ed4d51875325fe8c1
park chan <parkchan428@gmail.com>
2018-02-05 17:02:24 +0900

    many_to_many

--- django/many_to_many/admin.py
+++ django/many_to_many/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from many_to_many.models import *
+
 admin.site.register(Pizza)
 admin.site.register(Topping)
+
+admin.site.register(Post)
+admin.site.register(User)
+admin.site.register(PostLike)
--- django/many_to_many/models.py
+++ django/many_to_many/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 
 
@@ -16,3 +17,31 @@ class Pizza(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    like_users = models.ManyToManyField('User', through='PostLike', related_name='like_posts')
+
+    def __str__(self):
+        return self.title
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class PostLike(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{title} {name} {date}'.format(
+            title = self.post.title,
+            name=self.user.name,
+            date=datetime.strftime(datetime.timezone.localtime
+                                   (self.created_date, '%Y, %m, %d')))
