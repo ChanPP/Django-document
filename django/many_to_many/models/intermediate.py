e3cb24c6b540b77f230f6a9e255de9bedbf90199
park chan <parkchan428@gmail.com>
2018-02-06 10:57:18 +0900

    many_to_many models에 패키지화

---
+++ django/many_to_many/models/intermediate.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    like_users = models.ManyToManyField('User', through='PostLike', related_name='like_posts')
+
+    def __str__(self):
+        return self.title
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class PostLike(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{title} {name} {date}'.format(
+            title = self.post.title,
+            name=self.user.name,
+            date=datetime.strftime(datetime.timezone.localtime
+                                   (self.created_date, '%Y, %m, %d')))
+__all__
